6a8e4c104412b4ba9ebcdb48dd9c4527a3e343d1
Tange <tantantatange@gmail.com>
2020-02-25 23:51:27 +0900

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by 丹下敦矢 on 2020/02/25.
+//  Copyright © 2020 丹下敦矢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
